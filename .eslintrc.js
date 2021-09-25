5bc6b4da3161d69b12bc32cb7d5be73deb6ae84b
Jackson <58273860+jchoyce@users.noreply.github.com>
2021-09-25 15:30:46 -0600

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-    env: {
-      commonjs: true,
-      node: true,
-      browser: true,
-      es6: true,
-      jest: true
+  env: {
+    commonjs: true,
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {},
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    globals: {},
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      ecmaVersion: 2020,
-      sourceType: 'module'
-    },
-    plugins: ['react', 'react-native'],
-    ignorePatterns: ['node_modules/'],
-    rules: {
-      'react/prop-types': 0,
-      camelcase: 0
-    },
-    settings: {
-      react: {
-        version: 'detect'
-      }
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-native'],
+  ignorePatterns: ['node_modules/'],
+  rules: {
+    'react/prop-types': 0,
+    camelcase: 0
+  },
+  settings: {
+    react: {
+      version: 'detect'
     }
   }
+}
