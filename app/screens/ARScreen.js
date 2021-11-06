9460bc0624132e0756623ef1550ae654b6189d17
Asher-W <85578685+Asher-W@users.noreply.github.com>
2021-11-06 14:53:22 -0700

    add friend indicators

--- app/screens/ARScreen.js
+++ app/screens/ARScreen.js
@@ -6,7 +6,57 @@ import firebase from 'firebase'
 import LocationComponent from '../components/LocationComponent'
 import {readLocation} from '../components/FirebaseComponent'
 
+import { Magnetometer } from 'expo-sensors'
+import * as Location from 'expo-location';
+
+const { height, width } = Dimensions.get('window');
+
+var keys = {"Tyler": "XAcuKHuAXib6JovnqaWpYrJMwRU2", "Alexa": "iRYUmGV2kEagUAazosuLO5UPjOu1", "Asher": "UwEJEtRgFZY0wc7rgBOcXpb4Dln2"}
+
 function ARScreen ({ navigation }) {
+  const [hasPermission, setHasPermission] = useState(null)
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [subscription, setSubscription] = useState(null);
+  const [magnetometer, setMagnetometer] = useState(0); 
+
+  
+
+  function angleBetween(thisPos, otherPos){
+    return Math.atan2(otherPos[1] - thisPos[0],otherPos[0] - thisPos[1])
+  }
+   
+  function angle (sensor, otherPos) {
+    let angle = 0;
+    if (sensor) {
+      let { x, y, z } = sensor;
+   
+      var d = Math.atan2(z, x)
+      var a = angleBetween(location, otherPos)
+     
+      if(a > 0 && d<a-Math.PI){d+=2*Math.PI}
+      else if(a < 0 && d>Math.PI+a){d-=2*Math.PI}
+   
+      angle = ((d-a)/Math.PI + 1/2 )*width;
+    }
+    console.log(angle)
+    return angle;
+  };
+
+  function renderFriends(){
+    var friends = []
+    var pos = []
+    for(let i of Object.keys(keys)){pos.push(readLocation(i))}
+    for(var i of pos){
+      friends.push(<View style={{backgroundColor: "#42f572",width:50,height:50,borderRadius:25,position: 'absolute', left: angle(magnetometer, i)-25}} key={i}></View>)
+    }
+    return friends
+  }
+
+
+
+
   const friendsPressHandler = () => {
     console.log('Friends Button Pressed!')
     navigation.navigate('FriendsListScreen')
@@ -17,14 +67,39 @@ function ARScreen ({ navigation }) {
     navigation.navigate('MapScreen')
   }
 
-  const [hasPermission, setHasPermission] = useState(null)
-  const [type, setType] = useState(Camera.Constants.Type.back)
+  
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync()
       setHasPermission(status === 'granted')
     })()
+
+
+
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+
+
+
+    setSubscription(
+      Magnetometer.addListener((data) => {
+        setMagnetometer(data);
+      })
+    );
+ 
+    return () => {
+      if(subscription) {subscription.remove();}
+      setSubscription(null);
+    };
   }, [])
 
   if (hasPermission === null) {
@@ -37,6 +112,7 @@ function ARScreen ({ navigation }) {
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} type={type}>
+        {renderFriends()}
         <View style={styles.touchView}> 
           <TouchableOpacity
             style={styles.mapsTouch}
