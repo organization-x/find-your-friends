4ec39d072417a097293baea81e15de5bc992677b
Tyler Du <vicguin65@gmail.com>
2021-11-13 11:37:32 -0800

    Rough try of firebase security (NOT WORKING)
    This is an attempt to try and have firebase restrict authorize to other accounts. Currently does not work.

--- app/screens/ARScreen.js
+++ app/screens/ARScreen.js
@@ -1,93 +1,86 @@
 import React, { useState, useEffect } from 'react'
-import { Text, Image, ImageBackground, StyleSheet, TouchableOpacity, View, Pressable, Dimensions ,SafeAreaView} from 'react-native'
-import { StatusBar } from 'expo-status-bar'
+import { Text, Image, StyleSheet, TouchableOpacity, View, Dimensions } from 'react-native'
 import { Camera } from 'expo-camera'
-import firebase from 'firebase'
 import LocationComponent from '../components/LocationComponent'
-import {readLocation} from '../components/FirebaseComponent'
+import { readLocation } from '../components/FirebaseComponent'
 
 import { Magnetometer } from 'expo-sensors'
-import * as Location from 'expo-location';
+import * as Location from 'expo-location'
 
-const { height, width } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window')
 
-var keys = {"Tyler": "XAcuKHuAXib6JovnqaWpYrJMwRU2", "Alexa": "iRYUmGV2kEagUAazosuLO5UPjOu1", "Asher": "UwEJEtRgFZY0wc7rgBOcXpb4Dln2"}
+const keys = { Tyler: 'XAcuKHuAXib6JovnqaWpYrJMwRU2', Alexa: 'iRYUmGV2kEagUAazosuLO5UPjOu1', Asher: 'UwEJEtRgFZY0wc7rgBOcXpb4Dln2' }
 
-var pos = []
+let pos = []
 
 function ARScreen ({ navigation }) {
   const [hasPermission, setHasPermission] = useState(null)
   const [type, setType] = useState(Camera.Constants.Type.back)
-  const [location, setLocation] = useState([47.773791,-122.206028]);
-  const [subscription, setSubscription] = useState(null);
-  const [magnetometer, setMagnetometer] = useState(0); 
+  const [location, setLocation] = useState([47.773791, -122.206028])
+  const [subscription, setSubscription] = useState(null)
+  const [magnetometer, setMagnetometer] = useState(0)
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync()
       setHasPermission(status === 'granted')
 
-      let { status2 } = await Location.requestForegroundPermissionsAsync();
+      const { status2 } = await Location.requestForegroundPermissionsAsync()
       if (status2 === 'granted') {
-        let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+        const location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true })
         console.log(location)
-        setLocation([location.coords.longitude, location.coords.latitude]);
+        setLocation([location.coords.longitude, location.coords.latitude])
       }
-      
-    })();
+    })()
 
     setSubscription(
       Magnetometer.addListener((data) => {
-        setMagnetometer(data);
+        setMagnetometer(data)
       })
-    );
- 
+    )
+
     return () => {
-      if(subscription) {subscription.remove();}
-      setSubscription(null);
-    };
+      if (subscription) { subscription.remove() }
+      setSubscription(null)
+    }
   }, [])
 
-  function angleBetween(thisPos, otherPos){
+  function angleBetween (thisPos, otherPos) {
     return Math.atan2(otherPos[1] - thisPos[0], otherPos[0] - thisPos[1])
   }
-   
+
   function angle (sensor, otherPos) {
-    let angle = 0;
+    let angle = 0
     if (sensor) {
-      let { x, y, z } = sensor;
-   
-      var d = Math.atan2(z, x)
-      var a = angleBetween(location, otherPos)
-     
-      if(a > 0 && d<a-Math.PI){d+=2*Math.PI}
-      else if(a < 0 && d>Math.PI+a){d-=2*Math.PI}
-   
-      angle = ((d-a)/Math.PI + 1/2 )*width;
+      const { x, y, z } = sensor
+
+      let d = Math.atan2(z, x)
+      const a = angleBetween(location, otherPos)
+
+      if (a > 0 && d < a - Math.PI) { d += 2 * Math.PI } else if (a < 0 && d > Math.PI + a) { d -= 2 * Math.PI }
+
+      angle = ((d - a) / Math.PI + 1 / 2) * width
     }
-    return angle;
-  };
+    return angle
+  }
 
-  function renderFriends(){
+  function renderFriends () {
     pos = []
-    for(let i of Object.values(keys)){pos.push(readLocation(i))}
+    for (const i of Object.values(keys)) { pos.push(readLocation(i)) }
 
-    var friends = []
-    for(var i in pos){
-      friends.push(<View key={i} style={{backgroundColor: "#42f572", width:50, height:50, borderRadius:25, position: 'absolute', left: Number(angle(magnetometer, pos[i]))-25, marginTop: 500}} ></View>)
+    const friends = []
+    for (const i in pos) {
+      friends.push(<View key={i} style={{ backgroundColor: '#42f572', width: 50, height: 50, borderRadius: 25, position: 'absolute', left: Number(angle(magnetometer, pos[i])) - 25, marginTop: 500 }} />)
     }
     return friends
   }
-
-
-
 
   const friendsPressHandler = () => {
     console.log('Friends Button Pressed!')
     navigation.navigate('FriendsListScreen')
   }
 
-  const mapsPressHandler = () =>{
+  const mapsPressHandler = () => {
     console.log('Maps Button Pressed!')
     navigation.navigate('MapScreen')
   }
@@ -98,11 +91,12 @@ function ARScreen ({ navigation }) {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>
   }
+
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} type={type}>
-        <View style={{backgroundColor: "#42f572", width:50,height:50, borderRadius:25, position: 'absolute', left: Number(angle(magnetometer, [-54,12]))-25}} ></View>
-        <View style={styles.touchView}> 
+        <View style={{ backgroundColor: '#42f572', width: 50, height: 50, borderRadius: 25, position: 'absolute', left: Number(angle(magnetometer, [-54, 12])) - 25 }} />
+        <View style={styles.touchView}>
           <TouchableOpacity
             style={styles.mapsTouch}
             onPress={mapsPressHandler}
@@ -122,7 +116,7 @@ function ARScreen ({ navigation }) {
             />
           </TouchableOpacity>
         </View>
-        <View styles = {styles.viewBottom}/>
+        <View styles={styles.viewBottom} />
         <LocationComponent />
       </Camera>
     </View>
@@ -134,49 +128,49 @@ const dim = Dimensions.get('screen').width / 100
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection:'column',
+    flexDirection: 'column',
     justifyContent: 'space-around'
   },
 
-  viewBottom:{
-    flex:1
+  viewBottom: {
+    flex: 1
   },
-  camera:{
-    flex:1,
-    flexDirection:'column',
-    justifyContent:"space-around",
-    alignItems:'center'
+  camera: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   },
-  touchView:{
-    flex:1,
-    flexDirection:'row',
-    top:dim*20,
-    
-  },  
-  settingsTouch:{
-    alignSelf:'flex-start',
-    left:dim*25
+  touchView: {
+    flex: 1,
+    flexDirection: 'row',
+    top: dim * 20
+
   },
-  mapsTouch:{
-    alignSelf:'flex-start',
-    right:dim*25
+  settingsTouch: {
+    alignSelf: 'flex-start',
+    left: dim * 25
+  },
+  mapsTouch: {
+    alignSelf: 'flex-start',
+    right: dim * 25
   },
   settingsBtn: {
     width: 50,
-    height: 50,
-    
+    height: 50
+
     // alignSelf: 'flex-end',
     // top: '162.5%',
-    //left: (dim * 75),
-    //top: (dim * 20)
+    // left: (dim * 75),
+    // top: (dim * 20)
   },
-  mapsBtn:{
-    width: 954/ 954*50, //Units are from
-    height: 1430/ 954*50,
+  mapsBtn: {
+    width: 954 / 954 * 50, // Units are from
+    height: 1430 / 954 * 50
     // alignSelf: 'flex-end',
     // top: '162.5%',
-    //left: (dim * 150),
-    //top: (dim * 20)
+    // left: (dim * 150),
+    // top: (dim * 20)
   },
   background: {
     flex: 1,
--- app/screens/FriendsListScreen.js
+++ app/screens/FriendsListScreen.js
@@ -1,17 +1,58 @@
-import React from 'react'
-import { Text, Button, ImageBackground, StyleSheet, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
+import { render } from 'react-dom'
+import { View, Text, TextInput, Button, ImageBackground, StyleSheet, SafeAreaView } from 'react-native'
+import { makeFriends, currentUser, getFriends, readLocation } from '../components/FirebaseComponent'
 
 function FriendsListScreen ({ navigation }) {
-  const ARScreenPressHandler = () => {
+  const [user, setUser] = useState('')
+  const [friends, setFriends] = useState(getFriends)
+  const [addedFriend, setAddedFriend] = useState('')
+  const [revealCode, setRevealCode] = useState(true)
+
+  const backButtonNavigator = () => {
     navigation.navigate('ARScreen')
   }
 
-  return (
+  const revealFriendCode = () => {
+    // This flips revealCode from true to false or false to true.
+    // The code is then revealed
 
+    setRevealCode(!revealCode)
+    if (revealCode) {
+      setUser('Here is your friend code: ' + currentUser())
+    } else {
+      setUser('')
+    }
+  }
+
+  const addFriendHandler = () => {
+    makeFriends(addedFriend)
+  }
+
+  return (
     <SafeAreaView style={styles.background}>
       <Button
-        onPress={ARScreenPressHandler}
-        title='Add Friends!'
+        onPress={backButtonNavigator}
+        title='Back Button'
+        style={styles.buttons}
+      />
+      <Button
+        onPress={revealFriendCode}
+        title='Reveal Friend Code'
+        style={styles.buttons}
+      />
+      <Text>{user}</Text>
+      <Text>All your friends: {friends}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Enter Friend ID!'
+        onChangeText={(val) => setAddedFriend(val)}
+      />
+      <Text>The friend you want to add is: {addedFriend}</Text>
+      <Button
+        onPress={addFriendHandler}
+        title='Add Friend!'
+        style={styles.buttons}
       />
 
     </SafeAreaView>
@@ -21,7 +62,19 @@ function FriendsListScreen ({ navigation }) {
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: 'green'
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center'
+  },
+  buttons: {
+    flex: 1
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'blue',
+    padding: 8,
+    margin: 10,
+    width: 200
   }
 })
 
